9b2f2a3917246b75042864c95fc09ca28406545d
Emediong Udoh <emediong5135@gmail.com>
2024-11-11 06:34:44 +0100

    link footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className='bg-gray-800 p-4 text-center text-sm text-white'>
+            Copyright &copy; {new Date().getFullYear()} SwiftHire. All rights
+            reserved. <br />
+            Built by Anietie Iniabasi Inyang
+        </footer>
+    );
+}
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 // Components import
 import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
 export default function Layout() {
     return (
         <>
             <Header />
             <Outlet />
+            <Footer />
         </>
     );
 }
